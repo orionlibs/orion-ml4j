68d27b1c2e444be2a2a24524e1920cd5bb44816d
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-09-04 15:40:36 +0100

    initial commit

--- src/main/java/io/github/orionlibs/orion_ml4j/OrionDomain.java
+++ src/main/java/io/github/orionlibs/orion_ml4j/OrionDomain.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.project_name;
+package io.github.orionlibs.orion_ml4j;
 
 public class OrionDomain
 {
--- src/test/java/io/github/orionlibs/orion_ml4j/ATest.java
+++ src/test/java/io/github/orionlibs/orion_ml4j/ATest.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.project_name;
+package io.github.orionlibs.orion_ml4j;
 
 import java.io.IOException;
 import java.util.TimeZone;
